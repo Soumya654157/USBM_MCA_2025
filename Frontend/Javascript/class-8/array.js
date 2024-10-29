5fe820b56a41468f3046a135b80b44454bbf546d
soumya <jsoumyaranjan108@gmail.com>
2024-10-29 22:41:34 +0530

    add file

---
+++ Frontend/Javascript/class-8/array.js
@@ -0,0 +1,63 @@
+// let newArray=[];
+// console.log(newArray);
+// console.log(typeof newArray);
+
+//  let name='pablo';
+//  let age=60;
+//  let profession='criminal';
+//  let
+
+//  let userArr=["pablo",60,"criminal",true];
+// console.log(userArr);
+
+// let numbers=[1 ,2 ,3 ,4 ,5];
+// console.log(numbers);
+
+
+
+
+// let newNumbers=
+
+
+
+//create an array and check that length
+
+// length
+
+// console.log(numbera.length);
+
+//accessing
+
+ let cities=["bbsr","ctc","bls","bdk"];
+ //console.log(cities[0]);
+
+//  let lastcity=cities.length -1;
+//  console.log(cities[lastcity]);
+
+ //bls
+//  console.log(cities[2]);
+
+//  console.log(cities[lastcity/2]);
+//  console.log(cities[lastcity-2]);
+// //modify:
+
+// let modifycity = "kendrapada";
+// cities[2]=modifycity;
+// console.log(cities);
+
+//add element
+//push()
+// console.log(cities);
+// let addcity = "jajpur";
+// cities.push(addcity);
+// console.log(cities);
+//remove element
+//pop()
+console.log(cities);
+cities.pop();
+console.log(cities);
+
+//shift()
+console.log(cities);
+cities.unshift("baripada");
+console.log(cities);
